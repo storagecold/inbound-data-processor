2408690d848393ba1176eff6093a9f8739d16d16
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2021-03-27 15:02:51 -0400

    Create CustomerProfileService.java

---
+++ src/main/java/com/cold/storage/inbound/data/processor/service/CustomerProfileService.java
@@ -0,0 +1,27 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerProfileService {
+    private final EligibilityLogger log = EligibilityLogger.getLogger(CustomerProfileService.class);
+    private CustomerProfileRepo repo;
+
+    @Autowired
+    public CustomerProfileService(CustomerProfileRepo repo) {
+        this.repo = repo;
+    }
+
+    public SubmitterDefinition getCustomerProfile(String fileId, String submitterId) throws RuntimeException {
+        log.with($ -> $.addMessage("Get SubmitterDefinition for fileId:" + fileId + ", submitterId:" + submitterId)).info(EligibilityLogStatus.PROCESS);
+
+        SubmitterDefinition submitterDefinition = repo.getCustomerProfile(fileId, submitterId);
+        log.with($ -> $.addMessage("Successfully Retrieve submitterDefinition for fileId:" + fileId + ", submitterId:" + submitterId)).info(EligibilityLogStatus.PROCESS);
+        return submitterDefinition;
+    }
+
+    public SubmitterFullDetail getSubmitterFullDetail(String fileId, String submitterId) throws RuntimeException {
+        SubmitterFullDetail submitterFullDetail = repo.getSubmitterFullDetail(fileId, submitterId);
+        log.with($ -> $.addMessage("Successfully retrieved submitterFullDetail for fileId:" + fileId + ", submitterId:" + submitterId)).info(EligibilityLogStatus.PROCESS);
+        return submitterFullDetail;
+    }
+}
