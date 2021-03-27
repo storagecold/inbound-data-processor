ec6af032759d793ef8d3c99ccf97bc4f4bb10e28
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2021-03-27 15:06:05 -0400

    Create Constants.java

---
+++ src/main/java/com/cold/storage/inbound/data/processor/utility/Constants.java
@@ -0,0 +1,43 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class Constants {
+    public static final String UHG_POLICY_NBRS = "3168504";
+    public static final String DOT_TRIG_EXT = ".trig";
+    public static final String GSF_EXT = "gsf";
+    public static final String GPS_EXT = "gps";
+    public static final String HIPAA_EXT = "hipaa";
+    public static final String JSON_EXT = "json";
+    public static final String GSF_TRIG_EXT = ".gsf.trig";
+    public static final String GPS_TRIG_EXT = ".gps.trig";
+    public static final String HIPAA_TRIG_EXT = ".hipaa.trig";
+    public static final String JSON_TRIG_EXT = ".json.trig";
+    public static final String ELIG_SYS_CD_GPS = "G";
+    public static final String ELIG_SYS_CD_ENI = "8";
+    public static final String LOB_ENI = "ENI";
+    public static final String LOB_MNR = "MNR";
+    public static final String MKT_SEG_CD_5 = "G";
+    public static final String LOCATION_CD_1 = "1";
+    public static final String LOCATION_CD_2 = "2";
+
+    public static final class Header {
+        public static final String CALLERAPP = "CALLERAPP";
+        public static final String CALLERAPP_NAME = "eligibility-lz-process";
+    }
+
+    //Archive Path Sub Folders
+    public static final String B2BPrcElg = "B2BPrcElg";
+    public static final String B2BPrcElgUHG = "B2BPrcElgUHG";
+    public static final String B2BPrcElgOPTONS = "B2BPrcElgOPTONS";
+    public static final String B2BPrcElgOPTLVL6 = "B2BPrcElgOPTLVL6";
+    public static final String B2BPrcElgOPT = "B2BPrcElgOPT";
+    public static final String B2BPrcElgONS = "B2BPrcElgONS";
+    public static final String B2BPrcElgLVL6 = "B2BPrcElgLVL6";
+    public static final String OOPMax_Feed = "OOPMax_Feed";
+    public static final String VBD = "VBD";
+    public static final String MARKET = "MARKET";
+    public static final String MNR = "MNR";
+
+    public static final String SUFFIX = "/";
+
+}
