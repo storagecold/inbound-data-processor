ae4238cde192da745dd25520f564a73f49f0f24d
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2021-03-27 15:04:08 -0400

    Create EmailService.java

---
+++ src/main/java/com/cold/storage/inbound/data/processor/service/EmailService.java
@@ -0,0 +1,50 @@
+@org.springframework.stereotype.Service
+public class EmailService {
+    private EligibilityLogger log = EligibilityLogger.getLogger(EmailService.class);
+
+    @Value("${emailNotification.uri}")
+    public String emailNotifUri;
+
+    @Value("${spring.email.supportEmail}")
+    private List<String> supportEmail ;
+
+    private WebClient webClient;
+
+    @PostConstruct
+    private void initRestClients() {
+
+        webClient = WebClient.builder()
+                .defaultHeader(org.springframework.http.HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(org.springframework.http.HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(Constants.Header.CALLERAPP,Constants.Header.CALLERAPP_NAME).build();
+    }
+
+    public void sendMail(EmailRequest emailRequest)  {
+        try {
+            log.with($ -> $.addMessage("Sending Email for File : " + emailRequest.getFileName())).info();
+
+            if (Objects.nonNull(emailRequest)){
+                log.with($ -> $.addMessage("Email sent for File : " + emailRequest.getFileName())).info();
+                String response = webClient.post().uri(emailNotifUri)
+                        .body(Mono.just(emailRequest), EmailRequest.class).retrieve().bodyToMono(String.class).block();
+            } else {
+                log.with($ -> $.addMessage("Email not sent for File : " + emailRequest.getFileName())).info();
+            }
+
+        } catch (Exception ex) {
+                log.with($ -> $.addMessage("Failed to send email after 3 retries for file : " + emailRequest.getFileName()+ ex.getMessage()))
+                        .error(EligibilityLogStatus.SENTFAILURE);
+            }
+    }
+
+    public void generateEmailRequest(String fileName, String subject, String emailMessage) {
+        EmailRequest emailRequest = new EmailRequest();
+
+        emailRequest.setFileName(fileName);
+        emailRequest.setSubject(subject);
+        emailRequest.setMessage(emailMessage);
+        emailRequest.setToEmails(supportEmail);
+        sendMail(emailRequest);
+
+    }
+}
