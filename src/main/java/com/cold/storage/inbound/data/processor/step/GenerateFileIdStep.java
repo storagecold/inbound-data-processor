6bc82dd5806fdd6b59f87027a029e4613debec74
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2021-03-27 15:08:09 -0400

    Create GenerateFileIdStep.java

---
+++ src/main/java/com/cold/storage/inbound/data/processor/step/GenerateFileIdStep.java
@@ -0,0 +1,19 @@
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenerateFileIdStep {
+	
+	String fileId;
+	
+	public String getNewFileId() {
+		fileId = UUID.randomUUID().toString();
+		return fileId;
+	}
+	
+	public String getPreviouslyGeneratedFileId() {
+		return fileId;
+	}
+
+}
